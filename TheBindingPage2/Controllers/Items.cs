b86b2db147d7dda8e25c323accebbe050fbf6936
marchter <marchuter@tsn.at>
2021-11-24 15:02:37 +0100

    first push

---
+++ TheBindingPage2/Controllers/Items.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBindingPage2.Controllers
+{
+    public class Items : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //TODO: Consum & Trinkets der na hinzufügen und views erstellen
+        public IActionResult Consumables()
+        {
+            return View();
+        }
+        public IActionResult Trinkets()
+        {
+            return View();
+        }
+    }
+}
